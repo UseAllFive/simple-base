0d80e766471bfd34052c9e3accca983b65fb3c04
Bret Morris <morris.bret@gmail.com>
2018-02-16 17:34:49 -0800

    Remove unncessary code and files

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,6 @@
-import 'babel-polyfill';
-import { TweenMax } from 'gsap';
-
 class App {
 
-  constructor() {
-    TweenMax.to('.test', 0.5, {
-      color: 'red'
-    });
-  }
+  constructor() {}
 }
 
 new App();
